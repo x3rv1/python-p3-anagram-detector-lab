721d45555c539558c35467bf2a2f1e6d869f9404
Peter Xervi <xervipeter@gmail.com>
2025-11-27 05:51:26 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        return [w for w in words if sorted(w.lower()) == sorted(self.word.lower())]
